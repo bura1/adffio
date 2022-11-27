7a928c8725b269e1e56149a4c6192657c51a4c5b
tburic <tburic@pop-os.localdomain>
2022-11-27 20:54:56 +0100

    set template structure (base, nav, content)

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -9,8 +9,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'dashboard')]
-    public function dashboard(): Response
+    public function main(): Response
     {
-        return $this->render('dashboard/dashboard.html.twig');
+        return $this->render('dashboard/main.html.twig');
+    }
+
+    #[Route('/apps', name: 'apps')]
+    public function apps(): Response
+    {
+        return $this->render('dashboard/apps.html.twig');
+    }
+
+    #[Route('/ads', name: 'ads')]
+    public function ads(): Response
+    {
+        return $this->render('dashboard/ads.html.twig');
+    }
+
+    #[Route('/user-settings', name: 'user-settings')]
+    public function userSettings(): Response
+    {
+        return $this->render('dashboard/user-settings.html.twig');
     }
 }
